bf190c19555dfd25f569426ddc01a13594af0f87
vvakame <vvakame+dev@gmail.com>
2017-03-07 23:35:56 +0900

    use jquery.terminal for rich ui

--- rustwasm/run-wasm.sh
+++ rustwasm/run-wasm.sh
@@ -5,12 +5,12 @@ emcc -v
 
 cargo build --target wasm32-unknown-emscripten
 
-rm -rf dist/
-mkdir dist/
+rm -rf dist/ && mkdir dist/
 cp static/* dist/
-cp target/wasm32-unknown-emscripten/debug/deps/step4*.wasm dist/lib.wasm
-# exclude .asm.js
+# emscripten's bootstrap
 cp target/wasm32-unknown-emscripten/debug/deps/step4*[0-9a-f].js dist/lib.js
+# wasm
+cp target/wasm32-unknown-emscripten/debug/deps/step4*.wasm dist/lib.wasm
 
 echo "open http://localhost:8080/"
 cd dist/ && python -m SimpleHTTPServer 8080
